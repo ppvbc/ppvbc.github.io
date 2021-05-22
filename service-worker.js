83ee639b671d369bcef9bb56d4e0e514030a01e1
Choon Hong Peck <chpeck@gmail.com>
2021-05-22 22:26:40 +0800

    Don't cache index.html for now

--- service-worker.js
+++ service-worker.js
@@ -18,10 +18,7 @@ const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'index.html',
-  './' // Alias for index.html
-];
+const PRECACHE_URLS = [ ];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
